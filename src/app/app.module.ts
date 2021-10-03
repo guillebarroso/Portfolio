db59dbbcd3541b6d371ba206da2ac6fca65a93ae
guillebarroso <guillebarroso98@gmail.com>
2021-10-03 19:29:39 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes,RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CvComponent } from './components/cv/cv.component';
+import { MeComponent } from './components/me/me.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectsComponent,
+    CvComponent,
+    MeComponent,
+    MainNavComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'cv', component: CvComponent},
+      {path: 'projects', component: ProjectsComponent},
+      {path: '', component: MeComponent},
+    ]),
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
